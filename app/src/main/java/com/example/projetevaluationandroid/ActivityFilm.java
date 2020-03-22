d6bb4d79b494da2e4548bf3343f6dd1634c09f6c
leo <leoguirauton@gmail.com>
2020-03-22 16:02:10 +0100

    creation des activity vides

---
+++ app/src/main/java/com/example/projetevaluationandroid/ActivityFilm.java
@@ -0,0 +1,14 @@
+package com.example.projetevaluationandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityFilm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film);
+    }
+}
